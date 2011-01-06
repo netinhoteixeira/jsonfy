6d0b8e48e46fa2b1c1ad19b328b8990870337cd7
fco.ernesto <fco.ernesto@dff70095-1bdb-c73f-09ba-3b02a4ddeeae>
2011-01-06 15:14:23 +0000

    First commit.

---
+++ jsonfy.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * jsonfy.php
+ *
+ * Create on 2011-01-04, 16:26
+ *
+ * @author Francisco Ernesto Teixeira <contato@netinho.info>
+ */
+
+require_once dirname(__FILE__) . '/jsonfy.class.php';
+
+// instatiate the object
+$jsonfy = JSONfy::getInstance();
+?>
